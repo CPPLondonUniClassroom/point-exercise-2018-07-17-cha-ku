08e2747510e0ac497a3bcb1ceb8d721103038589
Chaitanya Kukde <chaitanya.kukde@gmail.com>
2018-09-24 11:45:45 +0530

    Add operator overload definitons

--- point.cpp
+++ point.cpp
@@ -9,3 +9,35 @@ bool operator==(const point& lhs, const point& rhs)
     return lhs.x == rhs.x &&
             lhs.y == rhs.y;
 }
+
+bool operator!=(const point& lhs, const point& rhs)
+{
+    return lhs.x != rhs.x ||
+            lhs.y != rhs.y;
+}
+
+point operator+(const point& lhs, const point& rhs)
+{
+    return point{lhs.x + rhs.x, lhs.y + rhs.y};
+}
+
+point operator-(const point& lhs, const point& rhs)
+{
+    return point{lhs.x - rhs.x, lhs.y - rhs.y};
+}
+
+point operator+=(const point& val)
+{
+    return point{this.x += val.x, this.y += val.y};
+}
+
+
+point operator-=(const point& val)
+{
+    return point{this.x -= val.x, this.y -= val.y};
+}
+
+std::ostream& operator<<(const point& val)
+{
+    return std::ostream << val.x << " , " << val.y ;
+}
